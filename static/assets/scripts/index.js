c1229a56efe9dbb39833b2ee744719f1059a8b14
aperlol <156535594+aperlol@users.noreply.github.com>
2024-01-13 09:16:29 -0800

    Update index.js

--- static/assets/scripts/index.js
+++ static/assets/scripts/index.js
@@ -57,3 +57,16 @@ function images(value) {
       return true;
     return false;
   }
+
+var password;
+
+var pass1="cool";
+
+password=prompt('Please enter your password to view this page!',' ');
+
+if (password==pass1)
+  alert('Password Correct! Click OK to enter!');
+else
+   {
+    window.location="google.com";
+    }
